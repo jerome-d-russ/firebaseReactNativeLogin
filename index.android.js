446744d5978d98b759f4eff38fcbf9b4814ad428
Jerome Russ <jerome.d.russ+github@gmail.com>
2016-10-17 21:14:49 -0500

    Adding and Removing

--- index.android.js
+++ index.android.js
@@ -18,10 +18,15 @@
  const ListItem = require('./components/ListItem');
  const styles = require('./styles.js');
 
+ // Initialize Firebase
+ const firebaseConfig = {
+ };
+ const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 class firebaseReactNativeLogin extends Component {
   constructor(props) {
     super(props);
+    this.itemsRef = firebaseApp.database().ref('/items/');
     this.state = {
       dataSource: new ListView.DataSource({
         rowHasChanged: (row1, row2) => row1 !== row2,
@@ -36,21 +41,45 @@ class firebaseReactNativeLogin extends Component {
         <ListView dataSource={this.state.dataSource}
           renderRow={this._renderItem.bind(this)}
           style={styles.listview} />
-        <ActionButton title="Add" onpress={() => { } } />
+          <ActionButton title="Add" onPress={this._addItem.bind(this)}/>
       </View>
     );
   }
 
   componentDidMount() {
-    this.setState({
-      dataSource: this.state.dataSource.cloneWithRows([{ title: 'Pizza' }])
-    })
+    this.listenForItems(this.itemsRef);
   }
 
   _renderItem(item) {
     return (
-      <ListItem item={item} onpress={() => { } }/>
+      <ListItem item={item} onPress={() => {
+        this.itemsRef.child(item._key).remove();
+      } }/>
     );
+  }
+
+  listenForItems(itemsRef) {
+    itemsRef.on('value', (snap) => {
+
+      // get children as an array
+      var items = [];
+      ctr = 0;
+      snap.forEach((child) => {
+        items.push({
+          title: child.val().title,
+          _key: child.key
+        });
+      });
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+
+    });
+  }
+
+  _addItem() {
+    this.itemsRef.push({ title:"Hiya2be"})
   }
 }
 
